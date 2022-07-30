f379ceacb68fa82b598c1f55b01a89adab0d7e92
heppokofrontend <heppokofrontend@gmail.com>
2022-07-30 10:09:05 +0900

    :wrench: Update types

--- @types/global.d.ts
+++ @types/global.d.ts
@@ -1,5 +1,4 @@
 import 'react';
-
 declare global {
   // A type for the properties of a function component
   namespace React {
@@ -16,14 +15,17 @@ declare global {
   // A type for JSX markup
   namespace JSX {
     interface IntrinsicElements {
-      'code-block': React.DetailedHTMLProps<React.HTMLAttributes<HTMLPreElement>, HTMLPreElement> & {
+      'code-block': React.DetailedHTMLProps<
+        React.HTMLAttributes<HTMLPreElement>,
+        HTMLPreElement
+      > & {
         /** The accessible name of code block */
         label?: string | undefined;
         /** The Language name */
         language?: string | undefined;
         /** The flag to display the UI */
         controls?: boolean;
-      }
+      };
     }
   }
 }
